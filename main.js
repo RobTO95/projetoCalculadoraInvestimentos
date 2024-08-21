12ec5d5b5400896bb368d4e379c02506bd4fd080
RobTO95 <roberio.teixeira.oliveira@gmail.com>
2024-08-21 19:55:52 -0300

    Associamos a acao ao event submit

---
+++ main.js
@@ -0,0 +1,36 @@
+import { generateReturnsArray } from "./src/investimentGoals";
+
+// const calculateButton = document.getElementById("calculate-results");
+
+const form = document.getElementById("investment-form");
+
+function renderProgression(event) {
+	event.preventDefault();
+
+	// const startingAmount = Number(form["starting-amount"].value); // Recuperando o valor do campo pelo seu atributo 'name'
+
+	const startingAmount = Number(
+		document.getElementById("starting-amount").value
+	);
+	const additionalContribution = Number(
+		document.getElementById("additional-contribution").value
+	);
+	const timeAmount = Number(document.getElementById("time-amount").value);
+	const timeAmountPeriod = document.getElementById("time-amount-period").value; //
+	const returnRate = Number(document.getElementById("return-rate").value);
+	const returnRatePeriod = document.getElementById("evaluation-period").value; //
+	const taxRate = Number(document.getElementById("tax-rate").value);
+	const returnsArray = generateReturnsArray(
+		startingAmount,
+		timeAmount,
+		timeAmountPeriod,
+		additionalContribution,
+		returnRate,
+		returnRatePeriod
+	);
+	console.log(returnsArray);
+}
+
+// calculateButton.addEventListener("click", renderProgression);
+
+form.addEventListener("submit", renderProgression);
